5191e98f9f7724dcd718d5075f4f4ac73a0bec94
TRequire <70069027+TRequire@users.noreply.github.com>
2020-08-30 15:18:04 +0300

    Update knowTest.js
    adding some Questions

--- knowTest.js
+++ knowTest.js
@@ -24,11 +24,11 @@ var json = {
                 {
                     type: "radiogroup",
                     name: "Technical",
-                    title: "What requirement is violated (Hollow Brick: ASTM C 652, Grade SW, Class H40V (void areas between 25 and 40 percent of gross cross-sectional area, Type HBX.)?",
+                    title: "Technical requirements define what is required in the design, they do not define how to create the design?",
                     choices: [
-                        "C1", "C2", "C3", "C4"
+                        "C1 Necessary", "C2 Appropriate", "C3 Unambiguous", "C4 Complete"
                     ],
-                    correctAnswer: "C2"
+                    correctAnswer: "C2 Appropriate"
                 }
             ]
         }, {
@@ -36,11 +36,14 @@ var json = {
                 {
                     type: "radiogroup",
                     name: "Technical",
-                    title: "What requirement is violated (Hollow Brick: ASTM C 652, Grade SW, Class H40V (void areas between 25 and 40 percent of gross cross-sectional area, Type HBX.)?",
+                    title: "A requirement is necessary (needed in the set of requirements) if",
                     choices: [
-                        "C1", "C2", "C3", "C4"
+                        "A – the requirement can be removed and the remaining set will still result in the entity needs being satisfied.",
+                        "B – the intent of the requirement will be met by the implementation of other requirements", 
+                        "C – the author cannot communicate the reason for the requirement",
+                        "D – a requirement must be able to be traced to a source which could be one or more entity need(s) or higher-level allocated requirement"
                     ],
-                    correctAnswer: "C2"
+                    correctAnswer: "D – a requirement must be able to be traced to a source which could be one or more entity need(s) or higher-level allocated requiremen"
                 }
             ]
         }, {
@@ -48,11 +51,11 @@ var json = {
                 {
                     type: "radiogroup",
                     name: "Technical",
-                    title: "What requirement is violated (Hollow Brick: ASTM C 652, Grade SW, Class H40V (void areas between 25 and 40 percent of gross cross-sectional area, Type HBX.)?",
+                    title: "Which rule the following requirement is viloting (The Identity of the Customer shall be confirmed)?",
                     choices: [
-                        "C1", "C2", "C3", "C4"
+                        "R2 Use an Active Voice", "R4 Use Defined Terms", "R14 Use Correct Spelling", "R24 Do Not Use Generalizations"
                     ],
-                    correctAnswer: "C2"
+                    correctAnswer: "R2 Use an Active Voice"
                 }
             ]
         }, {
@@ -60,11 +63,11 @@ var json = {
                 {
                     type: "radiogroup",
                     name: "Technical",
-                    title: "What requirement is violated (Hollow Brick: ASTM C 652, Grade SW, Class H40V (void areas between 25 and 40 percent of gross cross-sectional area, Type HBX.)?",
+                    title: "What is the issue with the following requirement: (The Operation Logger shall record any warning messages produced by the system)?",
                     choices: [
-                        "C1", "C2", "C3", "C4"
+                        "R10 Avoid Open-Ended Clauses", "R17 Do Not Use (And/Or)", "R19 Use a Single Sentence", "R34 Use (EACH)"
                     ],
-                    correctAnswer: "C2"
+                    correctAnswer: "R34 Use (EACH)"
                 }
             ]
         }, {
